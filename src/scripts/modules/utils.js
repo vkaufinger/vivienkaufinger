7169c3324d272a3792ff90b8b33d6c6ab04d10b5
Vivien KAUFINGER <kaufinger.vivien@gmail.com>
2017-12-04 17:14:00 +0100

    Implement image preloader

--- src/scripts/modules/utils.js
+++ src/scripts/modules/utils.js
@@ -6,8 +6,11 @@
     */
     var myModule = function () {
         var FontFaceObserver = require('fontfaceobserver');
+        var preload = require('preload.js');
         var html = document.documentElement;
 
+
+        // Observe fonts loading before use it to don't block the rendering
         function fontLoading () {
             var font = new FontFaceObserver('League Spartan');
             font.load().then(function () {
@@ -15,13 +18,44 @@
             });
         }
 
+
+        // Preload all images on window load
+        var imagesLoading = {
+            srcSet: function () {
+               document.querySelectorAll('img[data-src]').forEach(function (image) {
+                    image.setAttribute('src', image.getAttribute('data-src'));
+                    image.removeAttribute('data-src');
+                });
+            },
+            init: function () {
+                var that = this;
+                preload(imagesToLoad).progress(function (percent, image) {
+                    if (percent === 100) {
+                        that.srcSet();
+                    }
+                    console.log(percent, image);
+                });
+            }
+        };
+
+
         function ready () {
             fontLoading();
+
+            window.addEventListener('load', function () {
+                imagesLoading.init();
+            });
+        }
+
+
+        function ajaxComplete () {
+            imagesLoading.srcSet();
         }
 
         return {
             name: globName,
-            ready: ready
+            ready: ready,
+            ajaxComplete: ajaxComplete
         };
     }();
 
