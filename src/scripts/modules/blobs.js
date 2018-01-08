6b2ef4fb8b81b454a2e7b39aadc22b7e7c147cd5
Vivien KAUFINGER <kaufinger.vivien@gmail.com>
2018-01-08 16:17:33 +0100

    Adjust blobs parameters + reduce shapes size on tablet and mobile

--- src/scripts/modules/blobs.js
+++ src/scripts/modules/blobs.js
@@ -32,11 +32,16 @@
                 this.size        = el.size;
                 this.position    = el.position;
                 this.scrollSpeed = el.scrollSpeed;
-                this.threshold   = Math.min(this.size[0], this.size[1]) / 2;
+                this.threshold   = Math.min(this.size[0], this.size[1]) * 0.5;
 
                 // Create a fit rect to define shape position
                 this.fitRect = new Path.Rectangle(this.position, this.size);
                 // this.fitRect.strokeColor = 'blue';
+
+                // Reduce shapes size with scale on tablet and mobile layouts
+                if (vw < 1024) {
+                    this.fitRect.scale(0.65);
+                }
 
                 // Create the shape
                 this.shapeGen();
@@ -61,7 +66,7 @@
             }
             getSegments () {
                 // Define rotation speed
-                const rotationMultiplicator = Math.min(this.size[0], this.size[1]) / getRandomInt(400, 600);
+                const rotationMultiplicator = Math.min(this.size[0], this.size[1]) / getRandomInt(400, 500);
 
                 this.settings = [];
 
@@ -131,11 +136,11 @@
         function dataShapes () {
             shapes = [
                 // Intro
-                { type: 'intro', path: 'M34.046,729.082 C235.896,1334.609 995.948,1519.606 1596.445,1452.280 C2250.954,1378.899 2810.577,901.647 2682.000,601.000 C2390.905,-79.657 -328.624,-358.885 34.046,729.082 Z', color: '#fc6964', size: [vw * 0.8, vh * 0.9], position: [vw * 0, vh * 0.1], scrollSpeed: 0.85 },
-                { type: 'intro', path: 'M522.699,88.286 C-42.317,256.813 -361.462,1054.541 698.164,1341.627 C1757.791,1628.712 2687.238,986.133 2674.884,544.512 C2662.531,102.893 1295.841,-142.316 522.699,88.286 Z', color: '#fc6964', size: [vw * 0.8, vh * 0.8], position: [vw * 0.27, vh * -0.2], scrollSpeed: 0.6 },
-                { type: 'intro', path: 'M777.178,105.256 C1678.812,-435.186 2415.895,1274.091 1317.056,1424.686 C564.690,1527.796 -83.253,1315.659 8.747,1004.662 C100.746,693.665 450.909,300.822 777.178,105.256 Z', color: '#3f555e', size: [vw * 0.5, vh * 0.9], position: [vw * 0.15, vh * -0.21], scrollSpeed: 1.1 },
-                { type: 'intro', path: 'M508.785,884.410 C-254.041,381.310 -241.252,-121.338 1114.208,26.082 C1534.076,71.748 1678.535,107.459 1728.184,331.266 C1892.251,1070.844 1138.742,1299.881 508.785,884.410 Z', color: '#a172f3', size: [vw * 0.6, vh * 0.5], position: [vw * 0.47, vh * 0.37], scrollSpeed: 1.3 },
-                { type: 'intro', path: 'M322.183,646.249 C646.445,681.893 789.835,527.365 793.317,375.852 C797.112,210.714 587.157,12.389 424.360,1.408 C55.791,-23.452 -260.427,582.208 322.183,646.249 Z', color: '#a172f3', size: [vw * 0.3, vh * 0.45], position: [vw * 0.07, vh * 0.9], scrollSpeed: 2 },
+                { section: 'intro', path: 'M34.046,729.082 C235.896,1334.609 995.948,1519.606 1596.445,1452.280 C2250.954,1378.899 2810.577,901.647 2682.000,601.000 C2390.905,-79.657 -328.624,-358.885 34.046,729.082 Z', color: '#fc6964', size: [vw * 0.8, vh * 0.9], position: [vw * 0, vh * 0.1], scrollSpeed: 0.85 },
+                { section: 'intro', path: 'M522.699,88.286 C-42.317,256.813 -361.462,1054.541 698.164,1341.627 C1757.791,1628.712 2687.238,986.133 2674.884,544.512 C2662.531,102.893 1295.841,-142.316 522.699,88.286 Z', color: '#fc6964', size: [vw * 0.8, vh * 0.8], position: [vw * 0.27, vh * -0.2], scrollSpeed: 0.6 },
+                { section: 'intro', path: 'M777.178,105.256 C1678.812,-435.186 2415.895,1274.091 1317.056,1424.686 C564.690,1527.796 -83.253,1315.659 8.747,1004.662 C100.746,693.665 450.909,300.822 777.178,105.256 Z', color: '#3f555e', size: [vw * 0.5, vh * 0.9], position: [vw * 0.15, vh * -0.21], scrollSpeed: 1.1 },
+                { section: 'intro', path: 'M508.785,884.410 C-254.041,381.310 -241.252,-121.338 1114.208,26.082 C1534.076,71.748 1678.535,107.459 1728.184,331.266 C1892.251,1070.844 1138.742,1299.881 508.785,884.410 Z', color: '#a172f3', size: [vw * 0.6, vh * 0.5], position: [vw * 0.47, vh * 0.37], scrollSpeed: 1.3 },
+                { section: 'intro', path: 'M322.183,646.249 C646.445,681.893 789.835,527.365 793.317,375.852 C797.112,210.714 587.157,12.389 424.360,1.408 C55.791,-23.452 -260.427,582.208 322.183,646.249 Z', color: '#a172f3', size: [vw * 0.3, vh * 0.45], position: [vw * 0.07, vh * 0.9], scrollSpeed: 2 },
 
                 // Projects
                 { path: 'M4.669,164.583 C21.259,220.635 153.569,285.238 228.003,208.398 C302.437,131.559 211.213,22.220 133.650,2.565 C56.087,-17.089 -18.030,87.882 4.669,164.583 Z', color: '#fc6964', size: [vw * 0.08, vh * 0.15], position: [vw * 0.26, vh * 2.1], scrollSpeed: 1.2 },
@@ -143,12 +148,12 @@
                 { path: 'M992.275,441.058 C1157.959,-45.335 747.331,-52.144 356.494,60.890 C52.517,148.802 -17.442,266.939 3.409,422.809 C72.314,937.890 814.451,963.094 992.275,441.058 Z', color: '#a172f3', size: [vw * 0.4, vh * 0.4], position: [vw * 0.6,  vh * 3.1], scrollSpeed: 0.9 },
 
                 // Experiences
-                { path: 'M3.072,97.604 C11.541,133.243 96.810,126.644 131.190,110.045 C179.609,86.667 119.379,-14.764 68.919,2.036 C30.135,14.947 -8.517,48.835 3.072,97.604 Z', color: '#eaeaea', size: [vw * 0.045, vh * 0.055], position: [vw * 0.183,  vh * 5.05], scrollSpeed: 1, hiddenOnMobile: true },
-                { path: 'M108.780,33.085 C127.335,58.070 80.700,90.776 52.992,95.615 C13.970,102.431 -20.866,15.792 14.968,4.159 C42.511,-4.781 83.389,-1.103 108.780,33.085 Z', color: '#a172f3', size: [vw * 0.035, vh * 0.045], position: [vw * 0.208,  vh * 5.05], scrollSpeed: 1, hiddenOnMobile: true },
-                { path: 'M3.072,97.604 C11.541,133.243 96.810,126.644 131.190,110.045 C179.609,86.667 119.379,-14.764 68.919,2.036 C30.135,14.947 -8.517,48.835 3.072,97.604 Z', color: '#eaeaea', size: [vw * 0.045, vh * 0.055], position: [vw * 0.362,  vh * 5.05], scrollSpeed: 1, hiddenOnMobile: true },
-                { path: 'M108.780,33.085 C127.335,58.070 80.700,90.776 52.992,95.615 C13.970,102.431 -20.866,15.792 14.968,4.159 C42.511,-4.781 83.389,-1.103 108.780,33.085 Z', color: '#a172f3', size: [vw * 0.035, vh * 0.045], position: [vw * 0.387,  vh * 5.05], scrollSpeed: 1, hiddenOnMobile: true },
-                { path: 'M3.072,97.604 C11.541,133.243 96.810,126.644 131.190,110.045 C179.609,86.667 119.379,-14.764 68.919,2.036 C30.135,14.947 -8.517,48.835 3.072,97.604 Z', color: '#eaeaea', size: [vw * 0.045, vh * 0.055], position: [vw * 0.53,  vh * 5.05], scrollSpeed: 1, hiddenOnMobile: true },
-                { path: 'M108.780,33.085 C127.335,58.070 80.700,90.776 52.992,95.615 C13.970,102.431 -20.866,15.792 14.968,4.159 C42.511,-4.781 83.389,-1.103 108.780,33.085 Z', color: '#a172f3', size: [vw * 0.035, vh * 0.045], position: [vw * 0.555,  vh * 5.05], scrollSpeed: 1, hiddenOnMobile: true },
+                { section: 'experiences', path: 'M3.072,97.604 C11.541,133.243 96.810,126.644 131.190,110.045 C179.609,86.667 119.379,-14.764 68.919,2.036 C30.135,14.947 -8.517,48.835 3.072,97.604 Z', color: '#eaeaea', size: [64.8, 42.9], position: [vw * 0.183,  vh * 5.05], scrollSpeed: 1 },
+                { section: 'experiences', path: 'M108.780,33.085 C127.335,58.070 80.700,90.776 52.992,95.615 C13.970,102.431 -20.866,15.792 14.968,4.159 C42.511,-4.781 83.389,-1.103 108.780,33.085 Z', color: '#a172f3', size: [50.4, 35.1], position: [vw * 0.208,  vh * 5.05], scrollSpeed: 1 },
+                { section: 'experiences', path: 'M3.072,97.604 C11.541,133.243 96.810,126.644 131.190,110.045 C179.609,86.667 119.379,-14.764 68.919,2.036 C30.135,14.947 -8.517,48.835 3.072,97.604 Z', color: '#eaeaea', size: [64.8, 42.9], position: [vw * 0.362,  vh * 5.05], scrollSpeed: 1 },
+                { section: 'experiences', path: 'M108.780,33.085 C127.335,58.070 80.700,90.776 52.992,95.615 C13.970,102.431 -20.866,15.792 14.968,4.159 C42.511,-4.781 83.389,-1.103 108.780,33.085 Z', color: '#a172f3', size: [50.4, 35.1], position: [vw * 0.387,  vh * 5.05], scrollSpeed: 1 },
+                {section: 'experiences',  path: 'M3.072,97.604 C11.541,133.243 96.810,126.644 131.190,110.045 C179.609,86.667 119.379,-14.764 68.919,2.036 C30.135,14.947 -8.517,48.835 3.072,97.604 Z', color: '#eaeaea', size: [64.8, 42.9], position: [vw * 0.53,  vh * 5.05], scrollSpeed: 1 },
+                { section: 'experiences', path: 'M108.780,33.085 C127.335,58.070 80.700,90.776 52.992,95.615 C13.970,102.431 -20.866,15.792 14.968,4.159 C42.511,-4.781 83.389,-1.103 108.780,33.085 Z', color: '#a172f3', size: [50.4, 35.1], position: [vw * 0.555,  vh * 5.05], scrollSpeed: 1 },
 
                 // Contact
                 { path: 'M992.275,441.059 C1157.959,-45.335 747.331,-52.143 356.494,60.890 C52.517,148.803 -17.442,266.939 3.409,422.810 C72.314,937.890 814.451,963.094 992.275,441.059 Z', color: '#f92b27', size: [vw * 0.325, vh * 0.4], position: [vw * 0.53,  vh * 4.42], scrollSpeed: 0.75 },
@@ -158,8 +163,7 @@
 
             // Remove specific shapes on tablet and mobile
             if (vw < 1200) {
-                var desktopShapes = shapes.filter(el => !el.hiddenOnMobile);
-                shapes = desktopShapes;
+                shapes = shapes.filter(el => el.section !== 'experiences');
             }
         }
 
@@ -183,7 +187,7 @@
 
         function sceneIntro () {
             var complete = false;
-            var introShapes = shapes.filter(el => el.type === 'intro');
+            var introShapes = shapes.filter(el => el.section === 'intro');
 
             introShapes.forEach((el, index) => {
                 TweenLite.fromTo(el.blob.group.position, 1.5, { y: -vh }, { delay: 0.1 * index, y: el.blob.scrollY, ease: Power4.easeOut, onComplete: () => {
@@ -217,7 +221,7 @@
             Path = scene.Path;
             Group = scene.Group;
             smooth = require('./utils.js').smooth;
-            mouseForce = 0.2;
+            mouseForce = 0.3;
             mousePoint = new Point(-1000, -1000);
             vw = scene.view.bounds.width;
             vh = scene.view.bounds.height;
