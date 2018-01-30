3705adf9ca6224e714ec264dd30e7056a8051e54
Vivien KAUFINGER <kaufinger.vivien@gmail.com>
2018-01-30 14:17:12 +0100

    Remplace dynamic data by static data for blobs dimension and position

--- src/scripts/modules/blobs.js
+++ src/scripts/modules/blobs.js
@@ -9,7 +9,23 @@
         var TweenMax = require('gsap/src/uncompressed/TweenMax.js');
         var body = document.body;
         var blobsAreReady = false;
-        var shapes;
+        var shapes = [
+            // Intro
+            { section: 'intro', path: 'M34.046,729.082 C235.896,1334.609 995.948,1519.606 1596.445,1452.280 C2250.954,1378.899 2810.577,901.647 2682.000,601.000 C2390.905,-79.657 -328.624,-358.885 34.046,729.082 Z', color: '#fc6964', size: [ 0.8, 0.9], position: [ 0, 0.1], scrollSpeed: 0.85 },
+            { section: 'intro', path: 'M522.699,88.286 C-42.317,256.813 -361.462,1054.541 698.164,1341.627 C1757.791,1628.712 2687.238,986.133 2674.884,544.512 C2662.531,102.893 1295.841,-142.316 522.699,88.286 Z', color: '#fc6964', size: [ 0.8, 0.8], position: [ 0.27, -0.2], scrollSpeed: 0.6 },
+            { section: 'intro', path: 'M777.178,105.256 C1678.812,-435.186 2415.895,1274.091 1317.056,1424.686 C564.690,1527.796 -83.253,1315.659 8.747,1004.662 C100.746,693.665 450.909,300.822 777.178,105.256 Z', color: '#3f555e', size: [ 0.5, 0.9], position: [ 0.15, -0.21], scrollSpeed: 1.1 },
+            { section: 'intro', path: 'M508.785,884.410 C-254.041,381.310 -241.252,-121.338 1114.208,26.082 C1534.076,71.748 1678.535,107.459 1728.184,331.266 C1892.251,1070.844 1138.742,1299.881 508.785,884.410 Z', color: '#a172f3', size: [ 0.6, 0.5], position: [ 0.47, 0.37], scrollSpeed: 1.3 },
+            { section: 'intro', path: 'M322.183,646.249 C646.445,681.893 789.835,527.365 793.317,375.852 C797.112,210.714 587.157,12.389 424.360,1.408 C55.791,-23.452 -260.427,582.208 322.183,646.249 Z', color: '#a172f3', size: [ 0.3, 0.45], position: [ 0.07, 0.9], scrollSpeed: 2 },
+
+            // Projects
+            { path: 'M4.669,164.583 C21.259,220.635 153.569,285.238 228.003,208.398 C302.437,131.559 211.213,22.220 133.650,2.565 C56.087,-17.089 -18.030,87.882 4.669,164.583 Z', color: '#fc6964', size: [ 0.08, 0.15], position: [ 0.26, 2.1], scrollSpeed: 1.2 },
+            { path: 'M52.165,8.622 C-3.182,25.082 -34.445,102.991 69.353,131.030 C173.152,159.067 264.199,96.310 262.989,53.179 C261.779,10.049 127.900,-13.899 52.165,8.622 Z', color: '#3f555e', size: [ 0.05, 0.085], position: [ 0.65, 3.6], scrollSpeed: 1.1 },
+            { path: 'M992.275,441.058 C1157.959,-45.335 747.331,-52.144 356.494,60.890 C52.517,148.802 -17.442,266.939 3.409,422.809 C72.314,937.890 814.451,963.094 992.275,441.058 Z', color: '#a172f3', size: [ 0.4, 0.4], position: [ 0.6,  3.1], scrollSpeed: 0.9 },
+
+            // Contact
+            { path: 'M992.275,441.059 C1157.959,-45.335 747.331,-52.143 356.494,60.890 C52.517,148.803 -17.442,266.939 3.409,422.810 C72.314,937.890 814.451,963.094 992.275,441.059 Z', color: '#f92b27', size: [ 0.325, 0.4], position: [ 0.53,  4.42], scrollSpeed: 0.75 },
+            { path: 'M34.046,729.082 C235.897,1334.608 995.948,1519.605 1596.445,1452.279 C2250.954,1378.899 2810.577,901.647 2682.000,601.000 C2390.905,-79.658 -328.624,-358.885 34.046,729.082 Z', color: '#eaeaea', size: [ 0.8, 0.7], position: [ 0.45,  3.3], scrollSpeed: 0.5 }
+        ];
         var scene;
         var view;
         var Point;
@@ -18,8 +34,6 @@
         var smooth;
         var mouseForce;
         var mousePoint;
-        var vw;
-        var vh;
 
         function getRandomInt(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -27,10 +41,13 @@
 
         class Blob {
             constructor (el) {
+                const vw = scene.view.bounds.width;
+                const vh = scene.view.bounds.height;
+
                 this.path        = el.path;
                 this.color       = el.color;
-                this.size        = el.size;
-                this.position    = el.position;
+                this.size        = [el.size[0] * vw, el.size[1] * vh];
+                this.position    = [el.position[0] * vw, el.position[1] * vh];
                 this.scrollSpeed = el.scrollSpeed;
                 this.threshold   = Math.min(this.size[0], this.size[1]) * 0.5;
 
@@ -128,28 +145,6 @@
         }
 
 
-        function dataShapes () {
-            shapes = [
-                // Intro
-                { section: 'intro', path: 'M34.046,729.082 C235.896,1334.609 995.948,1519.606 1596.445,1452.280 C2250.954,1378.899 2810.577,901.647 2682.000,601.000 C2390.905,-79.657 -328.624,-358.885 34.046,729.082 Z', color: '#fc6964', size: [vw * 0.8, vh * 0.9], position: [vw * 0, vh * 0.1], scrollSpeed: 0.85 },
-                { section: 'intro', path: 'M522.699,88.286 C-42.317,256.813 -361.462,1054.541 698.164,1341.627 C1757.791,1628.712 2687.238,986.133 2674.884,544.512 C2662.531,102.893 1295.841,-142.316 522.699,88.286 Z', color: '#fc6964', size: [vw * 0.8, vh * 0.8], position: [vw * 0.27, vh * -0.2], scrollSpeed: 0.6 },
-                { section: 'intro', path: 'M777.178,105.256 C1678.812,-435.186 2415.895,1274.091 1317.056,1424.686 C564.690,1527.796 -83.253,1315.659 8.747,1004.662 C100.746,693.665 450.909,300.822 777.178,105.256 Z', color: '#3f555e', size: [vw * 0.5, vh * 0.9], position: [vw * 0.15, vh * -0.21], scrollSpeed: 1.1 },
-                { section: 'intro', path: 'M508.785,884.410 C-254.041,381.310 -241.252,-121.338 1114.208,26.082 C1534.076,71.748 1678.535,107.459 1728.184,331.266 C1892.251,1070.844 1138.742,1299.881 508.785,884.410 Z', color: '#a172f3', size: [vw * 0.6, vh * 0.5], position: [vw * 0.47, vh * 0.37], scrollSpeed: 1.3 },
-                { section: 'intro', path: 'M322.183,646.249 C646.445,681.893 789.835,527.365 793.317,375.852 C797.112,210.714 587.157,12.389 424.360,1.408 C55.791,-23.452 -260.427,582.208 322.183,646.249 Z', color: '#a172f3', size: [vw * 0.3, vh * 0.45], position: [vw * 0.07, vh * 0.9], scrollSpeed: 2 },
-
-                // Projects
-                { path: 'M4.669,164.583 C21.259,220.635 153.569,285.238 228.003,208.398 C302.437,131.559 211.213,22.220 133.650,2.565 C56.087,-17.089 -18.030,87.882 4.669,164.583 Z', color: '#fc6964', size: [vw * 0.08, vh * 0.15], position: [vw * 0.26, vh * 2.1], scrollSpeed: 1.2 },
-                { path: 'M52.165,8.622 C-3.182,25.082 -34.445,102.991 69.353,131.030 C173.152,159.067 264.199,96.310 262.989,53.179 C261.779,10.049 127.900,-13.899 52.165,8.622 Z', color: '#3f555e', size: [vw * 0.05, vh * 0.085], position: [vw * 0.65, vh * 3.6], scrollSpeed: 1.1 },
-                { path: 'M992.275,441.058 C1157.959,-45.335 747.331,-52.144 356.494,60.890 C52.517,148.802 -17.442,266.939 3.409,422.809 C72.314,937.890 814.451,963.094 992.275,441.058 Z', color: '#a172f3', size: [vw * 0.4, vh * 0.4], position: [vw * 0.6,  vh * 3.1], scrollSpeed: 0.9 },
-
-                // Contact
-                { path: 'M992.275,441.059 C1157.959,-45.335 747.331,-52.143 356.494,60.890 C52.517,148.803 -17.442,266.939 3.409,422.810 C72.314,937.890 814.451,963.094 992.275,441.059 Z', color: '#f92b27', size: [vw * 0.325, vh * 0.4], position: [vw * 0.53,  vh * 4.42], scrollSpeed: 0.75 },
-                { path: 'M34.046,729.082 C235.897,1334.608 995.948,1519.605 1596.445,1452.279 C2250.954,1378.899 2810.577,901.647 2682.000,601.000 C2390.905,-79.658 -328.624,-358.885 34.046,729.082 Z', color: '#eaeaea', size: [vw * 0.8, vh * 0.7], position: [vw * 0.45,  vh * 3.3], scrollSpeed: 0.5 }
-
-            ];
-        }
-
-
         function sceneRAF () {
             view.onFrame = function (e) {
                 shapes.forEach(function (el) {
@@ -171,7 +166,7 @@
             var introShapes = shapes.filter(el => el.section === 'intro');
 
             introShapes.forEach((el, index) => {
-                TweenMax.fromTo(el.blob.group.position, 1.5, { y: -vh }, { delay: 0.1 * index, y: el.blob.scrollY, ease: Power4.easeOut, onComplete: () => {
+                TweenMax.fromTo(el.blob.group.position, 1.5, { y: -scene.view.size.height }, { delay: 0.1 * index, y: el.blob.scrollY, ease: Power4.easeOut, onComplete: () => {
                     if (blobsAreReady) {
                         return;
                     }
@@ -207,10 +202,6 @@
             smooth = require('./utils.js').smooth;
             mouseForce = 0.3;
             mousePoint = new Point(-1000, -1000);
-            vw = scene.view.bounds.width;
-            vh = scene.view.bounds.height;
-
-            dataShapes();
 
             shapes.forEach((el) => {
                 el.blob = new Blob(el);
@@ -230,18 +221,11 @@
 
 
         function resize () {
-            vw = scene.view.bounds.width;
-            vh = scene.view.bounds.height;
 
             // Redraw shapes in scene after view resizing
-            shapes.forEach(function (el) {
-                el.blob.clear();
-            });
-
-            dataShapes();
-
-            shapes.forEach(function (el) {
-                el.blob = new Blob(el);
+            shapes.forEach((shape) => {
+                shape.blob.clear();
+                shape.blob = new Blob(shape);
             });
         }
 
